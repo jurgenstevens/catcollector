29cd269886f0f1a393416ef3eab968be151dafe2
jurgenstevens <jurgen.stevens@yahoo.com>
2022-10-17 21:19:03 -0500

    Set up admin portal/superuser.

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+  path('admin/', admin.site.urls),
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('cats/', views.cats_index, name='index'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,23 +1,6 @@
 
 from django.shortcuts import render
-
-# Add the following import
-from django.http import HttpResponse
-
-
-# Add the Cat class & list and view function below the imports
-class Cat:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-cats = [
-  Cat('Mija', 'calico', 'very needy', 10),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+from .models import Cat
 
 # Define the home view
 def home(request):
@@ -27,4 +10,5 @@ def about(request):
   return render(request, 'about.html')
 
 def cats_index(request):
+  cats = Cat.objects.all()
   return render(request, 'cats/index.html', { 'cats': cats })
