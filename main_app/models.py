eb789674f4ad9a45a45eb7f980db21a1673917b6
jurgenstevens <jurgen.stevens@yahoo.com>
2022-10-19 00:59:55 -0500

    Add feeding time stamp.

--- main_app/models.py
+++ main_app/models.py
@@ -36,3 +36,6 @@ class Feeding(models.Model):
     def __str__(self):
         # Nice method for obtaining the friendly value of a Field.choice
         return f"{self.get_meal_display()} on {self.date}  # type: ignore  # type: ignore"
+
+    class Meta:
+        ordering = ['-date']
