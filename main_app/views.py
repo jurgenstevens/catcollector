132d60061b0328ecdc2f7473c80512577de563b0
jurgenstevens <jurgen.stevens@yahoo.com>
2022-10-19 00:52:42 -0500

    Include add_feeding function in views.

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
 from .forms import FeedingForm
@@ -15,9 +15,9 @@ def cats_index(request):
   cats = Cat.objects.all()
   return render(request, 'cats/index.html', { 'cats': cats })
 
-def cats_detail(request, cat_id):
-  cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', { 'cat': cat })
+# def cats_detail(request, cat_id):  # type: ignore
+#   cat = Cat.objects.get(id=cat_id)
+#   return render(request, 'cats/detail.html', { 'cat': cat })
 
 class CatCreate(CreateView):
   model = Cat
@@ -41,3 +41,16 @@ def cats_detail(request, cat_id):
     # include the cat and feeding_form in the context
     'cat': cat, 'feeding_form': feeding_form
   })
+
+
+def add_feeding(request, cat_id):
+  # create the ModelForm using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)
